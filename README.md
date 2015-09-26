10000f1ec4be658440470aa2f8d97a5f55ef4bbf
Aleksey <astynax84@gmail.com>
2015-09-26 20:43:17 +0300

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+## Dialogui
+
+**Dialogui** - library, providing a simple UI, interacting with user through command line.
+
+### Installation
+
+```shell
+$ stack install
+```
+
+### Examples
+
+Example applications with different implementations of UI can be builded this way:
+```shell
+$ stack build --flag dialogui:buildexamples
+```
+
---
+++ examples/Echo.hs
@@ -0,0 +1,52 @@
+{-# LANGUAGE LambdaCase, CPP #-}
+
+module Main where
+
+import           UI.Dialogui
+# ifdef TK_GUI
+import           UI.Dialogui.HTk
+# else
+import           UI.Dialogui.TUI
+# endif
+
+
+main :: IO ()
+main =
+  let setup   = writeLn "Echo service is ready!"
+      runWith =
+# ifdef TK_GUI
+        runGUI "Echo"
+# else
+        runTUI
+# endif
+  in  runWith setup echo
+
+
+echo :: (Monad m) => Controller m Int
+echo = Controller { initialize  = return 1
+                  , finalize    = const $ return ()
+                  , communicate = (return .) . communicate' }
+  where
+    communicate' cnt = \case
+      ""   -> showHelp
+
+      ":?" -> showHelp
+
+      ":q" -> quit
+
+      ":r" -> clear <> clearInput
+
+      msg  -> setState (cnt + 1)
+              <> clearInput
+              <> write (show cnt)
+              <> write ": "
+              <> writeLn msg
+
+    showHelp = write
+               $ unlines [ "Use:"
+                         , " \":?\" to show this help"
+                         , " \":q\" to quit (Ctrl+D works same way)"
+                         , " \":r\" to reset state"
+                         , " <msg> to see an echo" ]
+    clearInput = setInput ""
+
