2c56e1f2920da686c585a40de4ac2ebd1ab47edb
Aleksey <astynax84@gmail.com>
2015-09-29 17:09:36 +0300

    Half of Dialogui.hs was haddockumented

--- src/UI/Dialogui.hs
+++ src/UI/Dialogui.hs
@@ -1,17 +1,31 @@
 module UI.Dialogui (
-  Output(..), Input, UI
-  , Action(..), Controller(..), Result, ScrollTarget(..)
+  -- * UI
 
-  , voidController, interact
+  -- ** Types
+  UI, Output(..), Input,
 
-  , (<>)
-  , quit, clear, setInput, setState
-  , write, writeLn
-  , scrollToBegin, scrollToEnd
+  -- * Controller
 
-  , State(finished, ctlState, uiState, uiInput)
-  , state, perform, finish
-  , modUIState, modCtlState
+  -- ** Types
+  Controller(..), Action(..), Result, ScrollTarget(..),
+
+  -- ** eDSL functions
+  (<>),
+  quit, clear, setInput, setState,
+  write, writeLn,
+  scrollToBegin, scrollToEnd,
+
+  -- ** Other functions
+  voidController, interact,
+
+  -- * Simple pure state
+
+  -- ** State type
+  State(finished, ctlState, uiState, uiInput),
+
+  -- ** Getters and modifiers
+  state, perform, finish,
+  modUIState, modCtlState
   ) where
 
 import           Data.Monoid ((<>))
@@ -22,10 +36,15 @@ data Output t = Output { writeTo     :: String -> t
                        , clearOutput :: t
                        , scrollTo    :: ScrollTarget -> t }
 
+{- | __U__ser __I__nterface which runs the 'Prelude.IO'-flavoured 'Controller'
+as 'Prelude.IO'-action.
+-}
 type UI a = Controller IO a -> IO ()
 
+-- | Output widget position to scroll to
 data ScrollTarget = BeginOfText | EndOfText
 
+-- | Controller's action
 data Action a = Write String
               | ClearOutput
               | SetInput String
@@ -33,26 +52,40 @@ data Action a = Write String
               | ScrollTo ScrollTarget
               | Quit
 
+-- | User input
 type Input = String
 
+-- | Result of evaluation of the user's 'Input' by 'Controller'.
 type Result a = [Action a]
 
-
+{- | Encapsulates the computation, dealing with 'Input'
+and producing some 'Result'.
+-}
 data Monad m => Controller m a =
   Controller { initialize  :: m a
+               -- ^ Computation producing the initial state
              , finalize    :: a -> m ()
-             , communicate :: a             -- previous state
-                           -> Input         -- current input
-                           -> m (Result a)  -- result
+               -- ^ Computation using the final state
+               -- to do something before quit
+             , communicate :: a -> Input -> m (Result a)
+               -- ^ Computation producing a couple of actions
+               -- as reaction to the user input
              }
 
 
--- | Pure State
-data State a b = State { finished :: Bool
-                       , uiInput  :: Maybe String
-                       , uiState  :: a
-                       , ctlState :: b
-                       }
+{- | Pure State encapsulating both the state of 'UI'
+and the state of 'Controller'.
+-}
+data State a b =
+  State { finished :: Bool
+          -- ^ "Was execution finished?"
+        , uiInput  :: Maybe String
+          -- ^ If @Just x@ then value of input widget will be set to x
+        , uiState  :: a
+          -- ^ New state of 'UI'
+        , ctlState :: b
+          -- ^ New state of 'Controller'
+        }
 
 
 -- | Controller which does nothing and have no state.
@@ -63,7 +96,7 @@ voidController = Controller { initialize  =                 return ()
                             , communicate = const $ const $ return [] }
 
 
--- | Analogue of ~Prelude.interact~ but runnable with UI
+-- | Analogue of the good old 'Prelude.interact' but runnable with 'UI'.
 interact :: (String -> String) -> Controller IO ()
 interact f = voidController { communicate = const $ \x -> return $ write $ f x }
 
@@ -88,12 +121,17 @@ scrollToEnd   = [ScrollTo EndOfText]
 
 -- Functions, helping to work with the pure state
 
-state :: a -> b -> State a b
+-- | Produces the initial 'State'.
+state :: a          -- ^ UI state
+      -> b          -- ^ Controller state
+      -> State a b
 state = State False Nothing
 
-perform :: Output (State a b -> State a b) -- output
-        -> [Action b]                      -- actions
-        -> State a b -> State a b          -- state transition
+-- | Performs the actions does some output and modifies the state.
+perform :: Output (State a b -> State a b) -- ^ UI output
+        -> [Action b]                      -- ^ Actions to perform
+        -> State a b                       -- ^ Old state
+        -> State a b                       -- ^ New state
 perform out = flip (foldl apply)
   where
     apply s _ | finished s = s
